b0c1469f244563fa88341e091f45a9c6e3aa904d
Mote Solo_Mon <68279892+Edotnd@users.noreply.github.com>
2020-11-18 15:29:58 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,34 +1,25 @@
+from flask_ngrok import run_with_ngrok
 from flask import Flask, render_template
 import pandas as pd
 
 app = Flask(__name__)
+run_with_ngrok(app)
 
-news_data = pd.read_table("upload_file.txt", error_bad_lines=False, sep='\t', names=['label', 'url', 'sentence'])
+shit_list = open('upload_file.txt', 'r')
 
-t_data=[]
-f_data=[]
+t_data = []
+f_data = []
 
-for i in range(0, news_data.shape[0]):
-    try:
-        n = news_data.loc[i]
-        if int(n[0]) == 1:
-            t_data.append([n[1], n[2]])
-        elif int(n[0]) == 0:
-            f_data.append([n[1], n[2]])
-    except:
-        news_data.drop([i])
+for line in shit_list.readlines():
+  dot = line.split('\t')
+  if int(dot[0]) == 1:
+    t_data.append([dot[1], dot[2]])
+  elif int(dot[0]) == 0:
+    f_data.append([dot[1], dot[2]])
 
 @app.route('/')
 def index():
     return render_template('index.html', t_data=t_data, f_data=f_data)
 
-@app.route('/true')
-def true():
-    return render_template('true.html', data=t_data)
-
-@app.route('/false')
-def false():
-    return render_template('false.html', data=f_data)
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="8080")
+    app.run()
